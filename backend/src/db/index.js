ecfcdfbd07fdf1158eb243c0336bbff11043af56
Soumyodeep-Dey <soumyodeepdey2003@gmail.com>
2025-04-21 20:47:28 +0530

    retrying

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${"watchly"}`);   
-        console.log(`MongoDB Connected: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        process.exit(1);
-    }
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/watchly`);
+    console.log(`MongoDB Connected: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
 };
 
 export { connectDB };
