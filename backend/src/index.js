1830cc2b9917e757a5fffc45f90eb88ac1aa7f10
Soumyodeep-Dey <soumyodeepdey2003@gmail.com>
2025-04-22 09:01:31 +0530

    checking

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,16 +2,14 @@ import dotenv from "dotenv";
 import { app } from "./app.js";
 import { connectDB } from "./db/index.js";
 
-dotenv.config({
-  path: "./.env",
-});
+dotenv.config({ path: "./.env" });
 
-const PORT = process.env.PORT || 8000; // Ensure the port is set to 8000
+const PORT = process.env.PORT || 5000;
 
 connectDB()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`); // Log server start message
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
